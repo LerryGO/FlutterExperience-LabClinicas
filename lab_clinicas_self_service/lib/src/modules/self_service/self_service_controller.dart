93e2d38554552947ddeac5f2290cda05d5e76b5f
Lerry Augusto <lerry.pirani@gmail.com>
2024-02-01 00:45:39 -0300

    D3A2 - Criando tela WHO I AM

--- lab_clinicas_self_service/lib/src/modules/self_service/self_service_controller.dart
+++ lab_clinicas_self_service/lib/src/modules/self_service/self_service_controller.dart
@@ -1,4 +1,5 @@
 import 'package:lab_clinicas_core/lab_clinicas_core.dart';
+import 'package:lab_clinicas_self_service/src/model/self_service_model.dart';
 import 'package:signals_flutter/signals_flutter.dart';
 
 enum FormSteps {
@@ -12,10 +13,27 @@ enum FormSteps {
 }
 
 class SelfServiceController with MessageStateMixin {
-  final _step = ValueSignal(FormSteps.none);
+  final _step = ValueSignal(
+    FormSteps.none,
+  );
+
+  var _model = const SelfServiceModel();
   FormSteps get step => _step();
 
   void startProcess() {
     _step.forceUpdate(FormSteps.whoIAm);
+  }
+
+  void goPatient() {
+    _step.forceUpdate(FormSteps.patient);
+  }
+
+  void setWhoIAmDataStepAndNext(String name, String lastName) {
+    _model = _model.copyWith(name: () => name, lastName: () => lastName);
+    _step.forceUpdate(FormSteps.findPatient);
+  }
+
+  void clearForm() {
+    _model = _model.clear();
   }
 }
--- lab_clinicas_self_service/lib/src/modules/self_service/who_i_am/who_i_am_page.dart
+++ lab_clinicas_self_service/lib/src/modules/self_service/who_i_am/who_i_am_page.dart
@@ -1,15 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:lab_clinicas_core/lab_clinicas_core.dart';
+import 'package:lab_clinicas_self_service/src/modules/self_service/self_service_controller.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:validatorless/validatorless.dart';
 
-class WhoIAmPage extends StatelessWidget {
+class WhoIAmPage extends StatefulWidget {
   const WhoIAmPage({super.key});
 
   @override
+  State<WhoIAmPage> createState() => _WhoIAmPageState();
+}
+
+class _WhoIAmPageState extends State<WhoIAmPage> {
+  final selfServiceController = Injector.get<SelfServiceController>();
+  final formKey = GlobalKey<FormState>();
+  final firstNameEC = TextEditingController();
+  final lastNameEC = TextEditingController();
+
+  @override
+  void dispose() {
+    firstNameEC.dispose();
+    lastNameEC.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('WHO I AM'),
-      ),
-      body: Container(),
+    final sizeOf = MediaQuery.sizeOf(context);
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) {
+        firstNameEC.text = '';
+        lastNameEC.text = '';
+        selfServiceController.clearForm();
+      },
+      child: Scaffold(
+          appBar: LabClinicasAppBar(
+            actions: [
+              PopupMenuButton(
+                child: const IconPopupMenuWidget(),
+                itemBuilder: (context) {
+                  return [
+                    const PopupMenuItem(
+                      value: 1,
+                      child: Text('Finalizar terminal'),
+                    )
+                  ];
+                },
+                onSelected: (value) async {
+                  if (value == 1) {
+                    final nav = Navigator.of(context);
+                    await SharedPreferences.getInstance()
+                        .then((sp) => sp.clear());
+
+                    nav.pushNamedAndRemoveUntil('/', (route) => false);
+                  }
+                },
+              )
+            ],
+          ),
+          body: LayoutBuilder(
+            builder: (_, constraints) {
+              return SingleChildScrollView(
+                child: Container(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/background_login.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(40),
+                      width: sizeOf.width * .8,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16)),
+                      child: Form(
+                        key: formKey,
+                        child: Column(
+                          children: [
+                            Image.asset('assets/images/logo_vertical.png'),
+                            const SizedBox(
+                              height: 48,
+                            ),
+                            const Text(
+                              'Bem-vindo',
+                              style: LabClinicasTheme.titleStyle,
+                            ),
+                            const SizedBox(height: 48),
+                            TextFormField(
+                              controller: firstNameEC,
+                              validator:
+                                  Validatorless.required('Nome obrigatório'),
+                              decoration: const InputDecoration(
+                                label: Text('Digite seu nome'),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 24,
+                            ),
+                            TextFormField(
+                              controller: lastNameEC,
+                              validator: Validatorless.required(
+                                  'Sobrenome obrigatório'),
+                              decoration: const InputDecoration(
+                                label: Text('Digite seu sobrenome'),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 24,
+                            ),
+                            SizedBox(
+                              width: sizeOf.width * .8,
+                              height: 48,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  final valid =
+                                      formKey.currentState?.validate() ?? false;
+                                  if (valid) {
+                                    selfServiceController
+                                        .setWhoIAmDataStepAndNext(
+                                            firstNameEC.text, lastNameEC.text);
+                                  }
+                                },
+                                child: const Text('Continuar'),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          )),
     );
   }
 }
